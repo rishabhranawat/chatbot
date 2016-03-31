0fd2ce1a6904de5c02f9e60eeb23dc3ec206d0e1
Eric Schles <ericschles@gmail.com>
2016-03-31 17:19:15 -0400

    updating

--- server/app/views.py
+++ server/app/views.py
@@ -1,17 +1,13 @@
 from app import app
 from flask import render_template, request, jsonify
 import shutil
-
+from chatbot import simplechat
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    if request.method == "POST":
-        print "got inside request post method"
-        import code
-        code.interact(local=locals())
-        return jsonify(request.get_json(force=True))
+    return request.values.to_dict()
         
-    return "worked"
+    
 #this will be the interface we use to track a whole bunch of statistics
 #we'll do visualizations around these statistics as well as dynamic text for analsyis and inspection
 @app.route("/dashboard",methods=["GET","POST"])
